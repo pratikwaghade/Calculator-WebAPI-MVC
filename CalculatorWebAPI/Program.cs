ac651d806196252b0622a22c17314cf9780d3f61
pratikwaghade <pratikwaghade31@gmail.com>
2025-07-04 14:41:39 +0530

    second commit

--- CalculatorWebAPI/Program.cs
+++ CalculatorWebAPI/Program.cs
@@ -7,19 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowAll", policy =>
-//    {
-//        policy.AllowAnyOrigin()
-//              .AllowAnyMethod()
-//              .AllowAnyHeader();
-//    });
-//});
 
 var app = builder.Build();
 
-//app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
